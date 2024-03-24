fd298559e9e0bce01345c32c77e3106f1ee47871
Vlad Petlyuk <vlad10112005@inbox.ru>
2024-03-24 10:52:51 +0300

    comment interface

--- src/components/ProductPage/ProductDescription.vue
+++ src/components/ProductPage/ProductDescription.vue
@@ -19,10 +19,21 @@
       <div class="comment-element">
         <div class="comment__header-section">
           <p class="commment__user">Vlad</p>
-          <div ref="comment-raiting" class="commment__raiting"></div>
+          <div ref="comment-raiting" class="commment__raiting">
+            <div v-for="star in raiting" :key="star" class="raiting-point raiting-point_active"></div>
+            <div v-for="star in (5 - raiting)" :key="star" class="raiting-point"></div>
+          </div>
         </div>
         <p class="comment-body">jfaslk;fjaasljfldsjflsd;fjsal;fjslkfjsdjfffffffffffffffffsalfjsadlfjsdlkafjlsdajf</p>
         <p class="comment-date">01.12.2023</p>
+      </div>
+      <div class="comment-input__section">
+        <textarea class="comment-input__text" name="" id="" placeholder="Input your comment"></textarea>
+        <div class="comment-input__stars-section commment__raiting">
+          <div :ref="`comment-star-${point}`" v-for="point in 5" :key="point"
+            class="raiting-point comment__raiting-point"></div>
+        </div>
+        <button class="comment-input__send-button">Send</button>
       </div>
     </div>
   </transition>
@@ -37,6 +48,7 @@ export default {
     return {
       activeElement: null,
       activeBlock: 'description',
+      raiting: 3,
     }
   },
   methods: {
@@ -50,21 +62,21 @@ export default {
       this.activeElement.classList.add('active');
       console.log(this.activeElement);
       setTimeout(() => {
-        this.showProductRaiting(2);
+        // this.showProductRaiting(2);
       }, 10)
     },
-    showProductRaiting(raiting) {
-      const raitingElements = [];
-      for (let i = 1; i <= raiting; i++) {
-        const raitingStarElement = `<div class="raiting-point raiting-point_active"></div>`;
-        this.$refs['comment-raiting'].insertAdjacentHTML('beforeEnd', raitingStarElement);
-      }
-      for (let j = 5 - raiting; j <= 5; j++) {
-        const raitingStarElementEmpty = `<div class="raiting-point"></div>`;
-        this.$refs['comment-raiting'].insertAdjacentHTML('beforeEnd', raitingStarElementEmpty);
-      }
-      return raitingElements.join('').slice(1, -1);
-    }
+    // showProductRaiting(raiting) {
+    //   const raitingElements = [];
+    //   for (let i = 1; i <= raiting; i++) {
+    //     const raitingStarElement = `<div class="raiting-point raiting-point_active"></div>`;
+    //     this.$refs['comment-raiting'].insertAdjacentHTML('beforeEnd', raitingStarElement);
+    //   }
+    //   for (let j = 5 - raiting; j <= 5; j++) {
+    //     const raitingStarElementEmpty = `<div class="raiting-point"></div>`;
+    //     this.$refs['comment-raiting'].insertAdjacentHTML('beforeEnd', raitingStarElementEmpty);
+    //   }
+    //   return raitingElements.join('').slice(1, -1);
+    // }
   },
   mounted() {
     setTimeout(() => {
@@ -76,15 +88,90 @@ export default {
 </script>
 
 <style scoped>
+.comment-input__text {
+  width: 100%;
+  height: 100px;
+  padding: 20px;
+  font-size: 16px;
+  border: 2px solid #46A358;
+  border-radius: 5px;
+  resize: none;
+}
+
+.comment-input__text:focus {
+  outline: none;
+  border: 2px solid #46A358;
+}
+
+.comment__raiting-point {
+  cursor: pointer;
+  transition: all .2s ease-in-out;
+}
+
+.comment__raiting-point:hover {
+  transform: scale(1.1);
+}
+
+.comment-input__send-button {
+  font-size: 18px;
+  color: #fff;
+  font-weight: 700;
+  background: #46A358;
+  padding: 10px 20px;
+  border-radius: 10px;
+  transition: all .3s ease;
+  cursor: pointer;
+  height: 45px;
+}
+
+.comment-input__send-button:hover {
+  transform: scale(1.1);
+}
+
+.comment-input__send-button::after {
+  content: "→";
+  font-weight: 700;
+  font-size: 18px;
+  color: #fff;
+  margin-left: 5px;
+}
+
+.raiting-point {
+  z-index: 10;
+  width: 30px;
+  height: 30px;
+  background-image: url("../../assets/img/cards/StarEmpty.svg");
+  background-size: 30px 30px;
+}
+
+.raiting-point_active {
+  z-index: 10;
+  width: 30px;
+  height: 30px;
+  background-image: url("../../assets/img/cards/Star.svg");
+  background-size: 30px 30px;
+}
+
 .comments-section {
   padding: 20px;
   height: 500px;
-  overflow: scroll;
+  overflow-x: hidden;
+  overflow-y: scroll;
+  display: grid;
+}
+
+.comment-input__section {
+  margin-top: 15%;
+  display: grid;
+  grid-template-columns: 8fr 2fr 1fr;
+  grid-column-gap: 40px;
+  padding: 20px;
 }
 
 .comment-element {
   background: #FBFBFB;
   border-radius: 20px;
+  height: fit-content;
   padding: 20px 40px;
 }
 
